dc98e62bddc6abaff9e7d4ea1dfbb35823b7c721
GLFspedine <gustavo.ferreira81@fatec.sp.gov.br>
2024-10-23 21:36:48 -0300

    push-23-10

--- my-app/src/app/carousel/carousel.component.ts
+++ my-app/src/app/carousel/carousel.component.ts
@@ -1,44 +1,13 @@
-import { Component, ViewChild } from '@angular/core';
-import { NgbCarousel, NgbCarouselModule, NgbSlideEvent, NgbSlideEventSource } from '@ng-bootstrap/ng-bootstrap';
-import { FormsModule } from '@angular/forms';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
-	selector: 'ngbd-carousel-pause',
+	selector: 'app-carousel',
 	standalone: true,
-	imports: [NgbCarouselModule, FormsModule],
+	imports: [CommonModule],
 	templateUrl: './carousel.component.html',
+  styleUrl: './carousel.component.css'
 })
-export class NgbdCarouselPause {
-	images = [62, 83, 466, 965, 982, 1043, 738].map((n) => `https://picsum.photos/id/${n}/900/500`);
-
-	paused = false;
-	unpauseOnArrow = false;
-	pauseOnIndicator = false;
-	pauseOnHover = true;
-	pauseOnFocus = true;
-
-	@ViewChild('carousel', { static: true })
-  carousel!: NgbCarousel;
-
-	togglePaused() {
-		if (this.paused) {
-			this.carousel.cycle();
-		} else {
-			this.carousel.pause();
-		}
-		this.paused = !this.paused;
-	}
-
-	onSlide(slideEvent: NgbSlideEvent) {
-		if (
-			this.unpauseOnArrow &&
-			slideEvent.paused &&
-			(slideEvent.source === NgbSlideEventSource.ARROW_LEFT || slideEvent.source === NgbSlideEventSource.ARROW_RIGHT)
-		) {
-			this.togglePaused();
-		}
-		if (this.pauseOnIndicator && !slideEvent.paused && slideEvent.source === NgbSlideEventSource.INDICATOR) {
-			this.togglePaused();
-		}
-	}
+export class CarouselComponent {
+    
 }
