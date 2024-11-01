60c90a58fed821c312ea48154b8b5a1fa80f3bc4
GLFspedine <gustavo.ferreira81@fatec.sp.gov.br>
2024-10-31 22:11:01 -0300

    31-10-24

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -3,14 +3,18 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
 import { NavBarComponent } from "./nav-bar/nav-bar.component";
 import { CarouselComponent } from "./carousel/carousel.component";
+import { IntroductionComponent } from "./introduction/introduction.component";
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, NavBarComponent, CarouselComponent],
+  imports: [RouterOutlet, HeaderComponent, NavBarComponent, CarouselComponent, IntroductionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'my-app';
 }
--- my-app/src/app/carousel/carousel.component.ts
+++ my-app/src/app/carousel/carousel.component.ts
@@ -1,13 +1,45 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { SlickCarouselModule } from 'ngx-slick-carousel';
 
 @Component({
-	selector: 'app-carousel',
-	standalone: true,
-	imports: [CommonModule],
-	templateUrl: './carousel.component.html',
+  selector: 'app-carousel',
+  standalone: true,
+  imports: [CommonModule, SlickCarouselModule],
+  templateUrl: './carousel.component.html',
   styleUrl: './carousel.component.css'
 })
 export class CarouselComponent {
-    
+  slide=[
+    {img:'./1.jpg'},
+    {img:'./2.jpg'},
+    {img:'./3.jpg'},
+  ]
+  slideConfig = {
+    "slidesToShow": 1,
+    "slidesToScroll": 1,
+    "autoplay": true,
+    "autoplaySpeed": 2000,
+    "infinite": true,
+    "responsive": [
+      {
+        breakpoint: 1024,
+        settings: {
+          "arrows": true,
+          "infinite": true,
+          "slidesToShow": 3,
+          "slidesToScroll": 3,
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          "arrows": true,
+          "infinite": true,
+          "slidesToShow": 2,
+          "slidesToScroll": 2
+        }
+      }
+    ]
+  };
 }
