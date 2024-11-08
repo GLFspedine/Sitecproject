3e506a478ab114d775f41810b7b2acb3d892842f
GLFspedine <gustavo.ferreira81@fatec.sp.gov.br>
2024-11-07 22:05:21 -0300

    07-11-14(2)

---
+++ my-app/src/app/infinite-carousel/infinite-carousel.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-infinite-carousel',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './infinite-carousel.component.html',
+  styleUrl: './infinite-carousel.component.css'
+})
+export class InfiniteCarouselComponent {
+
+}
