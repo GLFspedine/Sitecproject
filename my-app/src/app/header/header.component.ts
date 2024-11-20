c889bfe078dc1d00e02fac2f611a2a466799f20c
GLFspedine <gustavo.ferreira81@fatec.sp.gov.br>
2024-11-19 22:29:20 -0300

    19-11-24

--- my-app/src/app/header/header.component.ts
+++ my-app/src/app/header/header.component.ts
@@ -1,16 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { LoginComponent } from '../login/login.component';
+
 
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, LoginComponent],
+  imports: [CommonModule, ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
 export class HeaderComponent {
+[x: string]: any;
+
+sobre: any;
   openForm() {
     const formElement = document.getElementById('myForm');
     if (formElement) {
