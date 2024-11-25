2554573ad5c56fa57ae8fd6b5dee0326db140c9e
Ryan <ryanjobs234@gmail.com>
2024-11-25 16:06:02 -0300

    25-11-24 Tentei fazer o formReactiv

--- my-app/src/app/contact-form/contact-form.component.ts
+++ my-app/src/app/contact-form/contact-form.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
+import { FormGroup, ReactiveFormsModule, FormControl, Validators } from '@angular/forms'
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-contact-form',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './contact-form.component.html',
-  styleUrl: './contact-form.component.css'
+  styleUrl: './contact-form.component.css',
 })
 export class ContactFormComponent {
-
+  contact_forme = new FormGroup({
+    nome: new FormControl(' ', [Validators.required]),
+    email: new FormControl(' ', [Validators.required]),
+    assunto: new FormControl(' ', [Validators.required]),
+    descricao: new FormControl(' ', [Validators.required])
+  });
+  get nome() { return this.contact_forme.get('nome'); }
+  get email() { return this.contact_forme.get('email'); }
+  get assunto() { return this.contact_forme.get('assunto'); }
+  get descricao() { return this.contact_forme.get('descricao'); }
 }
