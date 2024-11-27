0717cabfd69fc8cc2736cc3d79bb4069083398fa
Ryan <ryanjobs234@gmail.com>
2024-11-27 19:51:52 -0300

    27-11-24 Atualização do formulário de contato.

--- my-app/src/app/login/login.component.ts
+++ my-app/src/app/login/login.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormGroup, ReactiveFormsModule, Validators, FormBuilder } from '@angular/forms'
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterModule],
-  templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  imports: [RouterModule, CommonModule, ReactiveFormsModule],
+  styleUrl: './login.component.css',
+  template: `<div routerLink="/login" class="form-popup" id="login">
+  <form  class="form-container">
+    <h1><img class="user-icon" src="/user.svg">Login <a ><img class="icon-close" src="/circle-close-multiple-svgrepo-com.svg" (click)="closeForm()"></a></h1>
+
+    <label for="email"><b>Email:</b></label>
+    <input type="text" placeholder="Enter Email" name="email" required>
+
+    <label for="psw"><b>Password:</b></label>
+    <input type="password" placeholder="Enter Password" name="psw" required>
+
+    <button type="submit" class="btn">Login</button>
+    <button  class="btn cancel" >Cadastrar</button>
+  </form>
+</div>`
 })
 export class LoginComponent {
   closeForm() {
